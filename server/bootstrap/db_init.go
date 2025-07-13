65f2d5a99f173061aa3781da734b8806e7162c69
Hermon Getachew <hermongetachew10@gmail.com>
2025-07-13 14:22:50 +0300

    feat: update db_init

--- server/bootstrap/db_init.go
+++ server/bootstrap/db_init.go
@@ -11,43 +11,43 @@ func initializeSchema(ctx context.Context, pool *pgxpool.Pool) error {
 	queries := []string{
 		`CREATE TABLE IF NOT EXISTS users (
 			id VARCHAR(255) PRIMARY KEY,
-			name VARCHAR(255) NOT NULL,
-			email VARCHAR(255) UNIQUE NOT NULL,
-			password VARCHAR(255) NOT NULL
+			name VARCHAR(255),
+			email VARCHAR(255) UNIQUE,
+			password VARCHAR(255)
 		)`,
 
 		`CREATE TABLE IF NOT EXISTS books (
 			id VARCHAR(255) PRIMARY KEY,
-			title VARCHAR(255) NOT NULL,
-			url VARCHAR(255)NOT NULL
+			title VARCHAR(255),
+			url VARCHAR(255)
 		)`,
 
 		`CREATE TABLE IF NOT EXISTS podcasts (
 		    id VARCHAR(255) PRIMARY KEY,
-		    title VARCHAR(255) NOT NULL,
+		    title VARCHAR(255),
 		    description VARCHAR(255),
-		    url VARCHAR(255) NOT NULL
+		    url VARCHAR(255)
 		)`,
 
 		`CREATE TABLE IF NOT EXISTS chapters (
 		    id VARCHAR(255) PRIMARY KEY,
-		    book_id VARCHAR(255) NOT NULL,
-		    chapter INTEGER NOT NULL,
-			title VARCHAR(255) NOT NULL,
+		    book_id VARCHAR(255),
+			chapter INTEGER,
+		    title VARCHAR(255),
 		    description VARCHAR(255),
 		    FOREIGN KEY (book_id) REFERENCES books(id)
 		)`,
 
 		`CREATE TABLE IF NOT EXISTS instances (
 		    id VARCHAR(255) PRIMARY KEY,
-		    user_id VARCHAR(255) NOT NULL,
-		    book_id VARCHAR(255) NOT NULL,
+		    user_id VARCHAR(255),
+		    book_id VARCHAR(255),
 		    chapter_id VARCHAR(255),
 		    podcast_id VARCHAR(255),
 		    FOREIGN KEY (user_id) REFERENCES users(id),
 		    FOREIGN KEY (book_id) REFERENCES books(id),
 		    FOREIGN KEY (chapter_id) REFERENCES chapters(id),
-		    FOREIGN KEY (podcast_id) REFERENCES podcasts(id)
+		    FOREIGN KEY (podcast_id) REFERENCES podcasts(id),
 		)`,
 	}
 	for _, query := range queries {
